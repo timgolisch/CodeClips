85e10c17a01eeff01fd3fae9328f827b88eb0f7c
Spam Head <spam@spamly.biz>
2026-05-08 15:07:50 -0400

    Minor fixes, updates to nuget packages, runtime, referenced dlls, use newest .net

--- dotnet/ReportViewer/Properties/AssemblyInfo.cs
+++ dotnet/ReportViewer/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ReportViewer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A utility for running RDL or RDLC report files")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ReportViewer")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
--- dotnet/ReportViewer/Properties/Settings.Designer.cs
+++ dotnet/ReportViewer/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ReportViewer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -35,7 +35,7 @@ namespace ReportViewer.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=SQLOLEDB;Server=localhost;Database=northwind")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=SQLOLEDB;Server=localhost;Database=northwind;")]
         public string DsnReport {
             get {
                 return ((string)(this["DsnReport"]));
